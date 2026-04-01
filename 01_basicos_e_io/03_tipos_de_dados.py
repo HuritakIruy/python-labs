2511a712c3a19fccacf3cbafe2ae70f1bcb9493e
Yuri Cristhian Ferreira Mota <yuricristhian390@gmail.com>
2026-04-01 20:14:29 -0300

    Add basic data types and print their types

---
+++ 01_basicos_e_io/03_tipos_de_dados.py
@@ -0,0 +1,11 @@
+nome = "Yuri"
+idade = 19
+altura = 1.75
+produto = "Leite"
+imovel = "Casa"
+print(type(nome))
+print(type(idade))
+print(type(altura))
+print(type(produto))
+print(type(imovel))
+print(f"{nome} tem {idade} anos e mede {altura}cm, atualmente está fora de sua {imovel} a procura de {produto}")
