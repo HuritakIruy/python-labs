df3a354f3c107e676383c5966bc63d28549451fb
Yuri Cristhian Ferreira Mota <yuricristhian390@gmail.com>
2026-04-01 20:23:10 -0300

    Add simple calculator for basic operations

---
+++ 02_operacoes_matematicas/04_calculadora_simples.py
@@ -0,0 +1,8 @@
+valor1= float(input("Digite um valor: "))
+valor2= float(input("Digite outro valor: "))
+soma = valor1 + valor2
+menos = valor1 - valor2
+vezes = valor1*valor2
+divisao = valor1/valor2
+print(f"{valor1}+{valor2}={soma}|| {valor1} - {valor2}={menos}|| {valor1}x{valor2}={vezes}|| {valor1}/{valor2}={divisao}")
+
