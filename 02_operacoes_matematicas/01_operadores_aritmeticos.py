5f26ea624e738a05de109fdcb3f32076da789c5d
Yuri Cristhian Ferreira Mota <yuricristhian390@gmail.com>
2026-04-01 20:19:45 -0300

    Create 01_operadores_aritmeticos.py

---
+++ 02_operacoes_matematicas/01_operadores_aritmeticos.py
@@ -0,0 +1,18 @@
+adicao = 10 + 10
+print(f"Adição -> 10+10 = {adicao}")
+
+subtracao = 10 - 5
+print(f"Subtração -> 10-5 = {subtracao}")
+
+multiplicacao = 10*2
+print(f"Multiplicação -> 10x2 = {multiplicacao}")
+
+divisao = 10/3
+print(f"Divisão -> 10/2 = {divisao}")
+
+divisaoInt = 10//3
+print(f"Divisão Inteira -> 10//3 = {divisaoInt}")
+
+potencia = 10 ** 2 
+print(f"Potencia -> 10² = {potencia}")
+
