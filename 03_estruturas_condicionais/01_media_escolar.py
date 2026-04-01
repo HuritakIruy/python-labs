b2bb681faeb1aa21e3402d0891b32ba099880a4b
Yuri Cristhian Ferreira Mota <yuricristhian390@gmail.com>
2026-04-01 20:25:04 -0300

    Add media escolar program for student approval check

---
+++ 03_estruturas_condicionais/01_media_escolar.py
@@ -0,0 +1,11 @@
+mediafinal = float(input("Digite a média Final: "))
+
+if mediafinal >=6:
+    print("Bloco A")
+    print("APROVADO")
+else:
+    print("Bloco B")
+    print("REPROVADO")
+
+print("Bloco C")
+print("Termina o programa")
