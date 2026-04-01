811583dfd97e3754c1378fd0eb283329fe1e4294
Yuri Cristhian Ferreira Mota <yuricristhian390@gmail.com>
2026-04-01 20:13:08 -0300

    Add input and formatting examples in Python

---
+++ 01_basicos_e_io/02_inputs_e_formatacao.py
@@ -0,0 +1,26 @@
+print(10,20,30)
+print("Eu sou uma String")
+print('tambem funciono assim')
+print(20, "colo um texto")
+
+print("Guarani", "Portuguesa", "Flamengo-SP", sep="\n")
+print("Processando informações", end=" ................ ")
+print("Concluído!")
+
+print("Essa é a Primeira linha \nEssa é a Segunda linha\nEssa é a Terceira linha")
+
+print("Nome \t\tIdade \tTime")
+print("Guilherm \t19 \tSantos")
+print("Bob Dylan \t20 \tCorinthians")
+print("Gabriel \t20 \tCorinthians")
+
+nome = input("Digite seu nome: ")
+print(nome)
+
+idade = int(input("Digite sua Idade: "))
+print(idade)
+
+altura = float(input("Digite sua altura: "))
+print(altura)
+
+print(f"Nome: {nome} Idade: {idade} Altura: {altura}")
